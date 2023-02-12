b4e1f7a24fc1c46e7e6f596b532584ac779743d4
Keino Chichester <keino.chichester@gmail.com>
2023-02-12 04:15:46 -0500

    work goddammit!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+import Nav from "./components/Nav.js"
+import Gallery from "./components/Gallery.js"
+
+
 
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:3200/listing")
+    .then(response => response.json())
+    .then(data => setListings(data))
+}, [])
+
+const [listingsArray, setListings] = useState([])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Gallery listingsArray = {listingsArray}/>
     </div>
   );
 }
