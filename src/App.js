bd50ab46efcc6d932a485a9659e78e9c382eec68
Ziru Zou <jiyucoding@yahoo.com>
2023-02-13 00:57:09 -0500

    login form

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Nav from "./components/Nav.js";
 import Home from "./pages/Home/Home";
 import Gallery from "./components/Gallery.js"
@@ -20,8 +20,18 @@ import UserPage from "./pages/UserPage/UserPage.js";
 
 
 function App() {
+
+  const navigate = useNavigate()
+
   const [listingsArray, setListings] = useState([])
   const [filter, setFilter] = useState("All")
+  const [user, setUser] = useState("")
+  const [isLoggedIn, setisLoggedIn] = useState(null)
+  
+  const admin = {
+    username: "admin",
+    password: "admin"
+  }
 
   useEffect(() => {
     fetch("http://localhost:3200/listing")
@@ -35,8 +45,19 @@ function App() {
     setFilter(prevFilter)
   }
 
+  function onLogin(loggedUser) {
+    if (loggedUser.username === admin.username && loggedUser.password === admin.password) {
+      setUser(loggedUser)
+      setisLoggedIn(!isLoggedIn)
+      navigate('/user')
+      
+    }else {
+      console.log("Login Failed")
+    }
+  }
+  
+  
   const filterResults = listingsArray?.filter(listing => {
-    
     if (filter === "All") {
       return listing
     } else {
@@ -51,7 +72,6 @@ function App() {
     <div className="App">
       <Nav />
       <Filter onHandleFilter={onHandleFilter} />
-
       <Routes>
 				<Route
 					exact
@@ -73,9 +93,8 @@ function App() {
 				/>
 
         <Route
-					exact
 					path="/login"
-					element={<Login />}
+					element={<Login onLogin={onLogin}/>}
 				/>
 
         
